0d952a76f700fcb648e3404f9437824b6c9613e1
Patryk Wydmuch <pvydmuch@gmail.com>
2025-04-20 18:26:15 +0200

    Url to service-b as env var

--- service-a/src/main/java/com/example/servicea/ServiceBClientConfig.java
+++ service-a/src/main/java/com/example/servicea/ServiceBClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.servicea;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -11,10 +12,16 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class ServiceBClientConfig {
 
+    @Value("${SERVICE_B_SERVICE_SERVICE_HOST}")
+    private String serviceBServiceHost;
+
+    @Value("${SERVICE_B_SERVICE_SERVICE_PORT}")
+    private Integer serviceBServicePort;
+
     @Bean
     ServiceBClient serviceBClient() {
         RestClient restClient = RestClient.builder()
-                .baseUrl("http://service-b-service:8080")
+                .baseUrl("http://" + serviceBServiceHost + ":" + serviceBServicePort)
                 .build();
         RestClientAdapter exchangeAdapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(exchangeAdapter).build();
